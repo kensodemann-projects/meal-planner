44cd13ff6019f81ccb02c551a27c5b0b1add8841
Ken Sodemann <ken.sodemann@gmail.com>
2025-11-09 06:46:02 -0600

    WIP - add the editor component

---
+++ src/components/NutritionalInformationEditor.vue
@@ -0,0 +1,19 @@
+<template>
+  <NutritionalInformationEditGrid v-model="editPortion" />
+</template>
+
+<script setup lang="ts">
+import { type Portion } from '@/models';
+import { ref } from 'vue';
+
+const props = defineProps<{ portion?: Portion }>();
+const editPortion = ref(
+  props.portion || {
+    sodium: 0,
+    sugar: 0,
+    carbs: 0,
+    fat: 0,
+    protein: 0,
+  },
+);
+</script>
---
+++ src/components/__tests__/NutritionalInformationEditor.spec.ts
@@ -0,0 +1,65 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, it } from 'vitest';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import NutritionalInformationEditor from '../NutritionalInformationEditor.vue';
+import type { Portion } from '@/models';
+import { TEST_PORTION } from '@/data/__tests__/test-data';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const mountComponent = (props: { portion?: Portion } = {}) =>
+  mount(NutritionalInformationEditor, { props, global: { plugins: [vuetify] } });
+
+const getInputs = (wrapper: ReturnType<typeof mountComponent>) => ({
+  unitsInput: wrapper.findComponent('[data-testid="units-input"]').find('input'),
+  unitOfMeasureInput: wrapper.findComponent('[data-testid="unit-of-measure-input"]').find('input'),
+  gramsInput: wrapper.findComponent('[data-testid="grams-input"]').find('input'),
+  caloriesInput: wrapper.findComponent('[data-testid="calories-input"]').find('input'),
+  sodiumInput: wrapper.findComponent('[data-testid="sodium-input"]').find('input'),
+  sugarInput: wrapper.findComponent('[data-testid="sugar-input"]').find('input'),
+  carbsInput: wrapper.findComponent('[data-testid="carbs-input"]').find('input'),
+  fatInput: wrapper.findComponent('[data-testid="fat-input"]').find('input'),
+  proteinInput: wrapper.findComponent('[data-testid="protein-input"]').find('input'),
+});
+
+describe('Nutritional Information Editor', () => {
+  it('renders', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  describe('for add', () => {
+    it('defaults the nutrients to zero', () => {
+      const wrapper = mountComponent();
+      const inputs = getInputs(wrapper);
+      expect(inputs.unitsInput.element.value).toBe('');
+      expect(inputs.unitOfMeasureInput.element.value).toBe('');
+      expect(inputs.gramsInput.element.value).toBe('');
+      expect(inputs.caloriesInput.element.value).toBe('');
+      expect(inputs.sodiumInput.element.value).toBe('0');
+      expect(inputs.sugarInput.element.value).toBe('0');
+      expect(inputs.carbsInput.element.value).toBe('0');
+      expect(inputs.fatInput.element.value).toBe('0');
+      expect(inputs.proteinInput.element.value).toBe('0');
+    });
+  });
+
+  describe('for update', () => {
+    it('initializes the data to the portion values', () => {
+      const wrapper = mountComponent({ portion: TEST_PORTION });
+      const inputs = getInputs(wrapper);
+      expect(inputs.unitsInput.element.value).toBe(TEST_PORTION.units.toString());
+      expect(inputs.gramsInput.element.value).toBe(TEST_PORTION.grams.toString());
+      expect(inputs.caloriesInput.element.value).toBe(TEST_PORTION.calories.toString());
+      expect(inputs.sodiumInput.element.value).toBe(TEST_PORTION.sodium.toString());
+      expect(inputs.sugarInput.element.value).toBe(TEST_PORTION.sugar.toString());
+      expect(inputs.carbsInput.element.value).toBe(TEST_PORTION.carbs.toString());
+      expect(inputs.fatInput.element.value).toBe(TEST_PORTION.fat.toString());
+      expect(inputs.proteinInput.element.value).toBe(TEST_PORTION.protein.toString());
+    });
+  });
+});
