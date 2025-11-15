1afcfa764ce1384f40921ab075170396cc1226a5
Ken Sodemann <ken.sodemann@gmail.com>
2025-11-15 14:08:00 -0600

    use the firebase emulator

--- src/core/firebase-app.ts
+++ src/core/firebase-app.ts
@@ -1,15 +1,13 @@
 import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 
 const config = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.PROD
-    ? 'kws-meal-planner.firebaseapp.com'
-    : 'my-daily-planner-development.firebaseapp.com',
-  projectId: import.meta.env.PROD ? 'kws-meal-planner' : 'my-daily-planner-development',
-  storageBucket: import.meta.env.PROD
-    ? 'kws-meal-planner.firebasestorage.com'
-    : 'my-daily-planner-development.firebasestorage.com',
+  authDomain: 'kws-meal-planner.firebaseapp.com',
+  projectId: 'kws-meal-planner',
+  storageBucket: 'kws-meal-planner.firebasestorage.com',
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || undefined,
@@ -17,6 +15,14 @@ const config = {
 
 const app = initializeApp(config);
 const analytics = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ? getAnalytics(app) : undefined;
+
+if (import.meta.env.DEV || window.location.hostname === 'localhost') {
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  console.log('Connecting to emulator...');
+  connectFirestoreEmulator(db, 'localhost', 8080);
+  connectAuthEmulator(auth, 'http://localhost:9099');
+}
 
 export const useFirebaseApp = () => {
   return { app, analytics };
