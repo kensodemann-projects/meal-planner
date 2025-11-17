503560bc097f9d0d7f1697e845c91a35e26897cb
Ken Sodemann <ken.sodemann@gmail.com>
2025-11-17 17:42:21 -0600

    refactor the expansion to remove items that fail

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,12 +3,33 @@ import { initializeApp } from 'firebase-admin/app';
 import { setGlobalOptions } from 'firebase-functions';
 import { onDocumentCreated } from 'firebase-functions/v2/firestore';
 import * as logger from 'firebase-functions/logger';
-import { convertFdcFoodItem, FoodItem } from '../../common';
+import { convertFdcFoodItem } from '../../common';
 
 const API_URL = 'https://api.nal.usda.gov/fdc/v1';
 
 initializeApp();
 setGlobalOptions({ maxInstances: 5 });
+
+const performFoodItemExpansion = async (fdcId: number, documentId: string) => {
+  const apiResponse = await fetch(
+    `${API_URL}/food/${fdcId}?api_key=${process.env.USDA_FDC_API_KEY}&nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=269.3&nutrients=307`,
+  );
+  if (!apiResponse.ok) {
+    throw new Error(`USDA FDC API returned status ${apiResponse.status}`);
+  }
+  const rawFoodItem = await apiResponse.json();
+  const foodItem = convertFdcFoodItem(rawFoodItem);
+
+  const db = getFirestore();
+  const docRef = db.collection('foods').doc(documentId);
+  await docRef.set(foodItem, { merge: true });
+};
+
+const removeFoodItem = async (documentId: string) => {
+  const db = getFirestore();
+  const docRef = db.collection('foods').doc(documentId);
+  await docRef.delete();
+};
 
 export const expandFoodItem = onDocumentCreated(
   { document: '/foods/{documentId}', secrets: ['USDA_FDC_API_KEY'] },
@@ -22,24 +43,11 @@ export const expandFoodItem = onDocumentCreated(
     logger.info(`Attempting to expand ${fdcId}`);
 
     let rawFoodItem: any;
-    let foodItem: Partial<FoodItem> = { name: 'There was an error expanding this item' };
     try {
-      const apiResponse = await fetch(
-        `${API_URL}/food/${fdcId}?api_key=${process.env.USDA_FDC_API_KEY}&nutrients=203&nutrients=204&nutrients=205&nutrients=208&nutrients=269.3&nutrients=307`,
-      );
-      if (!apiResponse.ok) {
-        throw new Error(`USDA FDC API returned status ${apiResponse.status}`);
-      }
-      rawFoodItem = await apiResponse.json();
-      foodItem = convertFdcFoodItem(rawFoodItem);
+      await performFoodItemExpansion(fdcId, event.params.documentId);
     } catch (err: unknown) {
       logger.error(`error expanding fdcId: ${fdcId}`, rawFoodItem, err);
+      await removeFoodItem(event.params.documentId);
     }
-
-    const documentId = event.params.documentId;
-    const db = getFirestore();
-    const docRef = db.collection('foods').doc(documentId);
-
-    return docRef.set(foodItem, { merge: true });
   },
 );
