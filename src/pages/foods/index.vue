37a244b72713a14e438029cd2d2c9f91fc547cd0
Ken Sodemann <ken.sodemann@gmail.com>
2025-11-17 18:19:12 -0600

    add child buttons to the main FAB

--- src/pages/foods/index.vue
+++ src/pages/foods/index.vue
@@ -4,15 +4,18 @@
     <v-list>
       <food-list-item v-for="food in foods" :key="food.fdcId" :food="food as FoodItem" @click="viewFoodItem" />
     </v-list>
-    <v-fab
-      color="primary"
-      icon="mdi-plus"
-      variant="tonal"
-      location="bottom end"
-      absolute
-      @click="router.push('foods/search-and-add')"
-      data-testid="add-button"
-    ></v-fab>
+    <v-fab color="primary" size="48" variant="flat" location="bottom end" absolute>
+      <v-icon>{{ open ? 'mdi-close' : 'mdi-plus' }}</v-icon>
+      <v-speed-dial v-model="open" location="top center" transition="fade-transition" activator="parent">
+        <v-btn key="1" color="secondary" variant="tonal" @click="router.push('foods/search-and-add')" icon>
+          <v-icon size="24">mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn key="2" color="secondary" variant="tonal" @click="router.push('foods/add')" icon>
+          <v-icon size="24">mdi-pencil</v-icon>
+        </v-btn>
+      </v-speed-dial>
+    </v-fab>
   </div>
 </template>
 
@@ -20,9 +23,11 @@
 import { type FoodItem } from '@meal-planner/common';
 import { useFoodsData } from '@/data/foods';
 import { useRouter } from 'vue-router';
+import { shallowRef } from 'vue';
 
 const { foods } = useFoodsData();
 const router = useRouter();
 
 const viewFoodItem = (food: FoodItem) => router.push(`foods/${food.id}`);
+const open = shallowRef(false);
 </script>
