e06e4ef91a8460533c3084424ae592353e2a1612
Ken Sodemann <ken.sodemann@gmail.com>
2025-11-24 04:01:35 -0600

    create the recipe editor

---
+++ src/components/RecipeEditor.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>recipe</div>
+</template>
+
+<script setup lang="ts">
+//
+</script>
---
+++ src/components/__tests__/RecipeEditor.spec.ts
@@ -0,0 +1,19 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, it } from 'vitest';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import RecipeEditor from '../RecipeEditor.vue';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const mountComponent = () => mount(RecipeEditor, { global: { plugins: [vuetify] } });
+
+describe('Recipe Editor', () => {
+  it('renders', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.exists()).toBe(true);
+  });
+});
