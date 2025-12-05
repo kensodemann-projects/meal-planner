96d65de40cd1e9f264326a722294c7306ea93d6a
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-04 20:38:12 -0600

    view a recipe

--- src/pages/recipes/[id]/index.vue
+++ src/pages/recipes/[id]/index.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <h1>Recipe Details Page</h1>
-    <p>This is the recipe details page for a specific recipe.</p>
+    <h1>{{ recipe?.name }}</h1>
     <RouterLink to="/recipes">Back to Recipes List</RouterLink>
   </div>
 </template>
 
 <script setup lang="ts">
-//
+import { useRoute } from 'vue-router';
+import { useRecipesData } from '@/data/recipes';
+import { ref } from 'vue';
+import type { Recipe } from '@/models/recipe';
+
+const recipe = ref<Recipe | null>(null);
+const { params } = useRoute();
+const id = (params as { id: string }).id;
+
+const { getRecipe } = useRecipesData();
+getRecipe(id).then((r) => {
+  recipe.value = r;
+});
 </script>
