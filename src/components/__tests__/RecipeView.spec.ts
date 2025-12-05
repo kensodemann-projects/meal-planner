6c8c5c5a56c54826ca90935b0b9456c78dbf044c
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-05 14:12:24 -0600

    WIP - add the recipe view

---
+++ src/components/__tests__/RecipeView.spec.ts
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import RecipeView from '../RecipeView.vue';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const mountComponent = (props = {}) => mount(RecipeView, { props, global: { plugins: [vuetify] } });
+
+describe('RecipeView', () => {
+  let wrapper: ReturnType<typeof mountComponent>;
+
+  afterEach(() => {
+    wrapper?.unmount();
+    vi.clearAllTimers();
+    try {
+      vi.useRealTimers();
+    } catch {}
+  });
+
+  it('renders', () => {
+    wrapper = mountComponent();
+    expect(wrapper.exists()).toBe(true);
+  });
+});
