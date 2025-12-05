8ab22231553726ef0b5ae9015075fcbe01f12358
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-05 16:42:04 -0600

    WIP - display the name

--- src/components/RecipeView.vue
+++ src/components/RecipeView.vue
@@ -1,7 +1,9 @@
 <template>
-  <div></div>
+  <h1>{{ recipe.name }}</h1>
 </template>
 
 <script setup lang="ts">
-//
+import type { Recipe } from '@/models/recipe';
+
+defineProps<{ recipe: Recipe }>();
 </script>
--- src/components/__tests__/RecipeView.spec.ts
+++ src/components/__tests__/RecipeView.spec.ts
@@ -1,15 +1,18 @@
+import { TEST_RECIPE } from '@/data/__tests__/test-data';
 import { mount } from '@vue/test-utils';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import RecipeView from '../RecipeView.vue';
+import type { Recipe } from '@/models/recipe';
 
 const vuetify = createVuetify({
   components,
   directives,
 });
-const mountComponent = (props = {}) => mount(RecipeView, { props, global: { plugins: [vuetify] } });
+const mountComponent = (props: { recipe: Recipe } = { recipe: { ...TEST_RECIPE, id: 'test-132' } }) =>
+  mount(RecipeView, { props, global: { plugins: [vuetify] } });
 
 describe('RecipeView', () => {
   let wrapper: ReturnType<typeof mountComponent>;
@@ -25,5 +28,12 @@ describe('RecipeView', () => {
   it('renders', () => {
     wrapper = mountComponent();
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders the name in an h1 header', () => {
+    wrapper = mountComponent();
+    const header = wrapper.find('h1');
+    expect(header.exists()).toBe(true);
+    expect(header.text()).toBe(TEST_RECIPE.name);
   });
 });
