981f00850ed692747a6e9c3ac7773d064d7395b0
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-06 06:46:22 -0600

    WIP - add the description

--- src/components/RecipeView.vue
+++ src/components/RecipeView.vue
@@ -1,24 +1,27 @@
 <template>
   <h1>{{ recipe.name }}</h1>
+
+  <div class="mt-4 mb-8 pre-formatted">{{ recipe.description }}</div>
+
   <div>
     <div><span class="font-weight-black">Difficulty:</span> {{ recipe.difficulty }}</div>
     <div><span class="font-weight-black">Category:</span> {{ recipe.category }}</div>
     <div><span class="font-weight-black">Servings:</span> {{ recipe.servings }}</div>
   </div>
 
-  <hr class="mt-4" />
+  <hr class="my-4" />
 
   <h2>Ingredients</h2>
 
-  <hr class="mt-4" />
+  <hr class="my-4" />
 
   <h2>Steps</h2>
 
-  <hr class="mt-4" />
+  <hr class="my-4" />
 
   <h2>Nutritional Information</h2>
 
-  <hr class="mt-4" />
+  <hr class="my-4" />
 </template>
 
 <script setup lang="ts">
