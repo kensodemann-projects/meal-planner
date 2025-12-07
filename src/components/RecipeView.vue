b17263660e7bc39ea2b8b8633c36ac4c572c0138
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-07 06:31:58 -0600

    Enhances recipe view component
    Adds distinct sections to organize recipe details and integrates the portion data component for nutritional information, improving readability and structure.
    Also adds `data-testid` attributes for testing purposes.

--- src/components/RecipeView.vue
+++ src/components/RecipeView.vue
@@ -1,17 +1,18 @@
 <template>
   <h1>{{ recipe.name }}</h1>
 
-  <div class="mt-4 mb-8 pre-formatted">{{ recipe.description }}</div>
-
-  <div>
-    <div><span class="font-weight-black">Difficulty:</span> {{ recipe.difficulty }}</div>
-    <div><span class="font-weight-black">Category:</span> {{ recipe.category }}</div>
-    <div><span class="font-weight-black">Servings:</span> {{ recipe.servings }}</div>
-  </div>
+  <section data-testid="description-section">
+    <div class="mt-4 mb-8 pre-formatted">{{ recipe.description }}</div>
+    <div>
+      <div><span class="font-weight-black">Difficulty:</span> {{ recipe.difficulty }}</div>
+      <div><span class="font-weight-black">Category:</span> {{ recipe.category }}</div>
+      <div><span class="font-weight-black">Servings:</span> {{ recipe.servings }}</div>
+    </div>
+  </section>
 
   <hr class="my-4" />
 
-  <section>
+  <section data-testid="ingredients-section">
     <h2>Ingredients</h2>
     <ul role="list" class="mt-2">
       <li v-for="ingredient in recipe.ingredients" :key="ingredient.id">
@@ -23,7 +24,7 @@
 
   <hr class="my-4" />
 
-  <section>
+  <section data-testid="steps-section">
     <h2>Steps</h2>
     <ol class="mt-2">
       <li v-for="step in recipe.steps" :key="step.id">
@@ -34,8 +35,9 @@
 
   <hr class="my-4" />
 
-  <section>
+  <section data-testid="nutritional-information-section">
     <h2>Nutritional Information</h2>
+    <PortionData :value="recipe" />
   </section>
 
   <hr class="my-4" />
@@ -43,17 +45,18 @@
 
 <script setup lang="ts">
 import type { Recipe } from '@/models/recipe';
+import PortionData from './PortionData.vue';
 
 defineProps<{ recipe: Recipe }>();
 </script>
 
 <style scoped>
-ul[role='list'] {
-  list-style-type: none;
-  padding-left: 1.5rem;
+ul {
+  list-style-type: square;
+  padding-left: 1rem;
 }
 
 ol {
-  padding-left: 1.5rem;
+  padding-left: 1rem;
 }
 </style>
