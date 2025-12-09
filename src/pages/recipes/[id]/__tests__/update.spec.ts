d50753cb78c6289a6d842f489a00d9ac08a06321
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-08 18:40:35 -0600

    Mocks vue-router and recipes data
    Adds mocks for `vue-router` and `useRecipesData` to isolate the component during testing. This ensures focused unit tests and prevents external dependencies from affecting test results.

--- src/pages/recipes/[id]/__tests__/update.spec.ts
+++ src/pages/recipes/[id]/__tests__/update.spec.ts
@@ -1,9 +1,15 @@
+import { TEST_RECIPE } from '@/data/__tests__/test-data';
+import { useRecipesData } from '@/data/recipes';
 import { mount } from '@vue/test-utils';
-import { afterEach, describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi, type Mock } from 'vitest';
+import { useRoute, useRouter } from 'vue-router';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import update from '../update.vue';
+
+vi.mock('vue-router');
+vi.mock('@/data/recipes');
 
 const vuetify = createVuetify({
   components,
@@ -13,6 +19,13 @@ const mountComponent = (props = {}) => mount(update, { props, global: { plugins:
 
 describe('update', () => {
   let wrapper: ReturnType<typeof mountComponent>;
+
+  beforeEach(() => {
+    const { getRecipe } = useRecipesData();
+    (useRoute as Mock).mockReturnValue({ params: { id: '88f933fiieo' } });
+    (useRouter as Mock).mockReturnValue({ replace: vi.fn() });
+    (getRecipe as Mock).mockResolvedValue({ ...TEST_RECIPE, id: '88f933fiieo' });
+  });
 
   afterEach(() => {
     wrapper?.unmount();
