6dfea1b9ce67fd9b58e6e13f543d93bfb9caeb39
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-08 18:54:36 -0600

    Adds update recipe page
    Creates the update recipe page, including fetching the recipe data and integrating the recipe editor component for modifying recipes.

--- src/pages/recipes/[id]/update.vue
+++ src/pages/recipes/[id]/update.vue
@@ -1,7 +1,26 @@
 <template>
-  <div></div>
+  <RecipeEditor v-if="recipe" :recipe="recipe" @cancel="onCancel" @save="onSave" />
 </template>
 
 <script setup lang="ts">
-//
+import RecipeEditor from '@/components/RecipeEditor.vue';
+import { useRecipesData } from '@/data/recipes';
+import type { Recipe } from '@/models/recipe';
+import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const router = useRouter();
+const { params } = useRoute();
+const id = (params as { id: string }).id;
+const recipe = ref<Recipe | null>();
+
+const { getRecipe, updateRecipe } = useRecipesData();
+getRecipe(id).then((r) => (recipe.value = r));
+
+const onCancel = () => router.replace(`/recipes/${recipe.value?.id}`);
+
+const onSave = async (item: Recipe) => {
+  await updateRecipe(item);
+  router.replace(`/recipes/${item.id}`);
+};
 </script>
