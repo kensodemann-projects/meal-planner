2d2a64cd3db318261b38a8ce9d9be96c540ca99c
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-26 13:29:32 -0600

    WIP - test the navigation

--- src/pages/planning/__tests__/index.spec.ts
+++ src/pages/planning/__tests__/index.spec.ts
@@ -1,13 +1,14 @@
-import { mount } from '@vue/test-utils';
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { flushPromises, mount } from '@vue/test-utils';
+import { afterEach, beforeEach, describe, expect, it, vi, type Mock } from 'vitest';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-
-vi.mock('@/data/settings');
-
 import IndexPage from '../index.vue';
 import { useSettingsData } from '@/data/settings';
+import { useRouter } from 'vue-router';
+
+vi.mock('@/data/settings');
+vi.mock('vue-router');
 
 const vuetify = createVuetify({
   components,
@@ -21,6 +22,9 @@ describe('Planning', () => {
   beforeEach(() => {
     vi.useFakeTimers();
     vi.setSystemTime(new Date('2025-12-25'));
+    (useRouter as Mock).mockReturnValue({
+      push: vi.fn(),
+    });
   });
 
   afterEach(() => {
@@ -37,5 +41,30 @@ describe('Planning', () => {
   it('gets the settings', async () => {
     wrapper = mountPage();
     expect(useSettingsData).toHaveBeenCalled();
+  });
+
+  describe('week cards', () => {
+    describe('next week', () => {
+      it('is the first card', async () => {
+        wrapper = mountPage();
+        await flushPromises();
+        const weekCards = wrapper.findAllComponents(components.VCard);
+        const title = weekCards[0]?.findComponent(components.VCardTitle);
+        expect(title?.text()).toBe('Next Week (Planning)');
+      });
+
+      it('navigates to the week details for next week', async () => {
+        const router = useRouter();
+        wrapper = mountPage();
+        await flushPromises();
+        const weekCards = wrapper.findAllComponents(components.VCard);
+        const nextWeekCard = weekCards[0]!;
+        await nextWeekCard.trigger('click');
+        expect(router.push).toHaveBeenCalledExactlyOnceWith({
+          path: './week',
+          query: { dt: '2025-12-29' },
+        });
+      });
+    });
   });
 });
