703168941d7a62875295ea30da962680798c54db
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-26 12:37:55 -0600

    WIP - add the day and week

---
+++ src/pages/planning/__tests__/week.spec.ts
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import week from '../week.vue';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const mountPage = (props = {}) => mount(week, { props, global: { plugins: [vuetify] } });
+
+describe('week', () => {
+  let wrapper: ReturnType<typeof mountPage>;
+
+  afterEach(() => {
+    wrapper?.unmount();
+    vi.clearAllTimers();
+    try {
+      vi.useRealTimers();
+    } catch {}
+  });
+
+  it('renders', () => {
+    wrapper = mountPage();
+    expect(wrapper.exists()).toBe(true);
+  });
+});
