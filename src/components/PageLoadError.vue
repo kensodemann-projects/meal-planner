8b2f2925e2d1bbf20d8737f843c77619b67aac21
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-27 12:50:43 -0600

    WIP - fill it out

--- src/components/PageLoadError.vue
+++ src/components/PageLoadError.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>Shit's broke yo!</div>
+  <div class="page-load-error text-center">
+    <v-icon class="death-icon" icon="mdi-death-star-variant" />
+    <div class="title">I find your lack of URL hacking skills disturbing.</div>
+    <div class="subtitle">That date is invalid</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 //
 </script>
+
+<style scoped>
+.death-icon {
+  font-size: 18rem;
+  font-weight: 300;
+}
+</style>
--- src/pages/shopping/index.vue
+++ src/pages/shopping/index.vue
@@ -1,5 +1,6 @@
 <template>
   <h2>This is the shopping list page</h2>
+  <PageLoadError />
 </template>
 
 <script lang="ts" setup>
