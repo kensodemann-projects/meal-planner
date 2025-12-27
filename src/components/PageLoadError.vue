c3e0c81e09eca02564b5f8a525c7f2c20e95ff34
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-27 12:33:33 -0600

    add a page load error for use in pages with parameters

---
+++ src/components/PageLoadError.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Shit's broke yo!</div>
+</template>
+
+<script setup lang="ts">
+//
+</script>
---
+++ src/components/__tests__/PageLoadError.spec.ts
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import PageLoadError from '../PageLoadError.vue';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const mountComponent = (props = {}) => mount(PageLoadError, { props, global: { plugins: [vuetify] } });
+
+describe('PageLoadError', () => {
+  let wrapper: ReturnType<typeof mountComponent>;
+
+  afterEach(() => {
+    wrapper?.unmount();
+    vi.clearAllTimers();
+    try {
+      vi.useRealTimers();
+    } catch {}
+  });
+
+  it('renders', () => {
+    wrapper = mountComponent();
+    expect(wrapper.exists()).toBe(true);
+  });
+});
