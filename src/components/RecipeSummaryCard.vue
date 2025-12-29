c4c34fbd8cfc0285e98fa529f7197388022bac2e
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-29 10:52:54 -0600

    WIP - flex

--- src/components/RecipeSummaryCard.vue
+++ src/components/RecipeSummaryCard.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-card @click="$emit('click', recipe)">
+  <v-card class="d-flex flex-column" @click="$emit('click', recipe)">
     <v-card-title>{{ recipe.name }}</v-card-title>
     <v-card-subtitle>
       {{ recipe.category }}
     </v-card-subtitle>
-    <v-card-text>
-      <div>
+    <v-card-text class="d-flex flex-column ga-4">
+      <div class="flex-grow-0">
         <v-chip size="small" color="primary">{{ recipe.cuisine }}</v-chip>
         <v-chip size="small" color="secondary">{{ recipe.difficulty }}</v-chip>
       </div>
-      <div class="my-4">{{ recipe.description }}</div>
-      <div>
+      <div class="flex-grow-1">{{ recipe.description }}</div>
+      <div class="flex-grow-0">
         <v-chip size="small" color="primary" prepend-icon="mdi-account-multiple-outline">{{ recipe.servings }}</v-chip>
         <v-chip size="small" color="secondary" prepend-icon="mdi-clock-time-four-outline">{{
           recipe.prepTimeMinutes + recipe.cookTimeMinutes
