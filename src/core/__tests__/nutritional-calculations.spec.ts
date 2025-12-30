8ad9f82bfd85384830ee0d68460bf48efaa0fa99
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-30 06:03:18 -0600

    Adds nutritional calculation service
    Implements calculation logic for meal and daily meal plan nutrients. Adds unit tests to verify the calculations.
    
    Relates to #123

--- src/core/__tests__/nutritional-calculations.spec.ts
+++ src/core/__tests__/nutritional-calculations.spec.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from 'vitest';
-import { mealNutrients } from '../nutritional-calculations';
+import { dailyMealPlanNutrients, mealNutrients } from '../nutritional-calculations';
 import { TEST_MEAL_PLAN } from '@/data/__tests__/test-data';
 
 describe('Nutritional Calculations', () => {
-  describe('for the meal', () => {
-    it('sums the data for the items in the meal', () => {
+  describe('for a meal', () => {
+    it('sums the data for the nutritional information for the data items', () => {
       const meal = TEST_MEAL_PLAN.meals[0]!;
       expect(mealNutrients(meal)).toEqual({
         calories: meal.items.reduce((sum, item) => sum + item.nutrition.calories, 0),
@@ -13,6 +13,21 @@ describe('Nutritional Calculations', () => {
         carbs: meal.items.reduce((sum, item) => sum + item.nutrition.carbs, 0),
         sugar: meal.items.reduce((sum, item) => sum + item.nutrition.sugar, 0),
         sodium: meal.items.reduce((sum, item) => sum + item.nutrition.sodium, 0),
+      });
+    });
+  });
+
+  describe('for a meal plan', () => {
+    it('sums the data for each meal', () => {
+      const mealPlan = TEST_MEAL_PLAN;
+      const nutrientsPerMeal = mealPlan.meals.map((meal) => mealNutrients(meal));
+      expect(dailyMealPlanNutrients(mealPlan)).toEqual({
+        calories: nutrientsPerMeal.reduce((sum, n) => sum + n.calories, 0),
+        protein: nutrientsPerMeal.reduce((sum, n) => sum + n.protein, 0),
+        fat: nutrientsPerMeal.reduce((sum, n) => sum + n.fat, 0),
+        carbs: nutrientsPerMeal.reduce((sum, n) => sum + n.carbs, 0),
+        sugar: nutrientsPerMeal.reduce((sum, n) => sum + n.sugar, 0),
+        sodium: nutrientsPerMeal.reduce((sum, n) => sum + n.sodium, 0),
       });
     });
   });
--- src/core/nutritional-calculations.ts
+++ src/core/nutritional-calculations.ts
@@ -1,5 +1,6 @@
 // import type { FoodItem } from '@/models/food';
 import type { Meal } from '@/models/meal';
+import type { MealPlan } from '@/models/meal-plan';
 // import type { MealPlan } from '@/models/meal-plan';
 import type { Nutrition } from '@/models/nutrition';
 // import type { UnitOfMeasure } from '@/models/unit-of-measure';
@@ -22,14 +23,28 @@ export const mealNutrients = (meal: Meal): Nutrition => ({
   sodium: meal.items.reduce((sum, item) => sum + item.nutrition.sodium, 0),
 });
 
-// export const dailyMealPlanNutrients = (mealPlan: MealPlan): Nutrition => ({
-//   calories: 0,
-//   protein: 0,
-//   fat: 0,
-//   carbs: 0,
-//   sugar: 0,
-//   sodium: 0,
-// });
+export const dailyMealPlanNutrients = (mealPlan: MealPlan): Nutrition =>
+  mealPlan.meals.reduce(
+    (nutrients, meal) => {
+      const forMeal = mealNutrients(meal);
+      return {
+        calories: nutrients.calories + forMeal.calories,
+        protein: nutrients.protein + forMeal.protein,
+        fat: nutrients.fat + forMeal.fat,
+        carbs: nutrients.carbs + forMeal.carbs,
+        sugar: nutrients.sugar + forMeal.sugar,
+        sodium: nutrients.sodium + forMeal.sodium,
+      };
+    },
+    {
+      calories: 0,
+      protein: 0,
+      fat: 0,
+      carbs: 0,
+      sugar: 0,
+      sodium: 0,
+    },
+  );
 
 // export const multiDayMealPlanNutrients = (mealPlans: MealPlan[]): Nutrition => ({
 //   calories: 0,
