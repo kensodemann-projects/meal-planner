170643a43cfa17a9e074e0836897d6d056d753de
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-30 06:12:45 -0600

    WIP - extract and reuse

--- src/core/nutritional-calculations.ts
+++ src/core/nutritional-calculations.ts
@@ -14,43 +14,26 @@ import type { Nutrition } from '@/models/nutrition';
 //   sodium: 0,
 // });
 
-export const mealNutrients = (meal: Meal): Nutrition => ({
-  calories: meal.items.reduce((sum, item) => sum + item.nutrition.calories, 0),
-  protein: meal.items.reduce((sum, item) => sum + item.nutrition.protein, 0),
-  fat: meal.items.reduce((sum, item) => sum + item.nutrition.fat, 0),
-  carbs: meal.items.reduce((sum, item) => sum + item.nutrition.carbs, 0),
-  sugar: meal.items.reduce((sum, item) => sum + item.nutrition.sugar, 0),
-  sodium: meal.items.reduce((sum, item) => sum + item.nutrition.sodium, 0),
+const zeroNutrition: Nutrition = {
+  calories: 0,
+  protein: 0,
+  fat: 0,
+  carbs: 0,
+  sugar: 0,
+  sodium: 0,
+};
+
+const sumNutrition = (a: Nutrition, b: Nutrition): Nutrition => ({
+  calories: a.calories + b.calories,
+  protein: a.protein + b.protein,
+  fat: a.fat + b.fat,
+  carbs: a.carbs + b.carbs,
+  sugar: a.sugar + b.sugar,
+  sodium: a.sodium + b.sodium,
 });
 
-export const dailyMealPlanNutrients = (mealPlan: MealPlan): Nutrition =>
-  mealPlan.meals.reduce(
-    (nutrients, meal) => {
-      const forMeal = mealNutrients(meal);
-      return {
-        calories: nutrients.calories + forMeal.calories,
-        protein: nutrients.protein + forMeal.protein,
-        fat: nutrients.fat + forMeal.fat,
-        carbs: nutrients.carbs + forMeal.carbs,
-        sugar: nutrients.sugar + forMeal.sugar,
-        sodium: nutrients.sodium + forMeal.sodium,
-      };
-    },
-    {
-      calories: 0,
-      protein: 0,
-      fat: 0,
-      carbs: 0,
-      sugar: 0,
-      sodium: 0,
-    },
-  );
+export const mealNutrients = (meal: Meal): Nutrition =>
+  meal.items.reduce((nutrients, item) => sumNutrition(nutrients, item.nutrition), zeroNutrition);
 
-// export const multiDayMealPlanNutrients = (mealPlans: MealPlan[]): Nutrition => ({
-//   calories: 0,
-//   protein: 0,
-//   fat: 0,
-//   carbs: 0,
-//   sugar: 0,
-//   sodium: 0,
-// });
+export const dailyMealPlanNutrients = (mealPlan: MealPlan): Nutrition =>
+  mealPlan.meals.reduce((nutrients, meal) => sumNutrition(nutrients, mealNutrients(meal)), zeroNutrition);
