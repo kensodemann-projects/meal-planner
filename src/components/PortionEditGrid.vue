324d93408d9a26cfe70c653d5e47592d3bdec42b
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-31 15:55:38 -0600

    WIP - use it

--- src/components/PortionEditGrid.vue
+++ src/components/PortionEditGrid.vue
@@ -30,65 +30,7 @@
       </v-col>
     </v-row>
 
-    <v-row>
-      <v-col cols="12" md="4">
-        <v-number-input
-          label="Calories"
-          v-model="calories"
-          :precision="null"
-          :rules="[validationRules.required]"
-          data-testid="calories-input"
-        ></v-number-input>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-number-input
-          label="Sodium (mg)"
-          v-model="sodium"
-          :precision="null"
-          :rules="[validationRules.required]"
-          data-testid="sodium-input"
-        ></v-number-input>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-number-input
-          label="Sugar (g)"
-          v-model="sugar"
-          :precision="null"
-          :rules="[validationRules.required]"
-          data-testid="sugar-input"
-        ></v-number-input>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col cols="12" md="4">
-        <v-number-input
-          label="Total Carbs (g)"
-          v-model="carbs"
-          :precision="null"
-          :rules="[validationRules.required]"
-          data-testid="carbs-input"
-        ></v-number-input>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-number-input
-          label="Fat (g)"
-          v-model="fat"
-          :precision="null"
-          :rules="[validationRules.required]"
-          data-testid="fat-input"
-        ></v-number-input>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-number-input
-          label="Protein (g)"
-          v-model="protein"
-          :precision="null"
-          :rules="[validationRules.required]"
-          data-testid="protein-input"
-        ></v-number-input>
-      </v-col>
-    </v-row>
+    <NutritionEditorRows v-model="portion" />
   </v-container>
 </template>
 
@@ -114,12 +56,6 @@ const createPortionField = <K extends keyof Partial<Portion>>(key: K) =>
 
 const units = createPortionField('units');
 const grams = createPortionField('grams');
-const calories = createPortionField('calories');
-const sodium = createPortionField('sodium');
-const sugar = createPortionField('sugar');
-const carbs = createPortionField('carbs');
-const fat = createPortionField('fat');
-const protein = createPortionField('protein');
 
 const unitOfMeasureId = computed({
   get: () => portion.value?.unitOfMeasure?.id,
