bcbdc9ce6430c3264518fbe73c09ed468fa3f0ef
Ken Sodemann <ken.sodemann@gmail.com>
2025-12-31 08:38:26 -0600

    WIP - add the buttons bar

--- src/components/MealEditor.vue
+++ src/components/MealEditor.vue
@@ -1,7 +1,31 @@
 <template>
-  <div></div>
+  <div>This is the meal editor</div>
+
+  <v-container fluid>
+    <v-row class="pa-4" justify="end">
+      <CancelButton class="mr-4" @click="$emit('cancel')" />
+      <SaveButton :disabled="!(valid && isModified)" @click="save" />
+    </v-row>
+  </v-container>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Meal } from '@/models/meal';
+import { computed, ref } from 'vue';
+
+const emit = defineEmits<{ (event: 'save', payload: Meal): void; (event: 'cancel'): void }>();
+const props = defineProps<{ meal?: Meal }>();
+
+const valid = ref(false);
+
+const isModified = computed((): boolean => false);
+
+const save = () => {
+  const mealToSave: Meal = {
+    // Populate meal data
+  };
+  emit('save', mealToSave);
+};
+</script>
 
 <style scoped></style>
