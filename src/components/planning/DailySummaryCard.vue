a3b969a419a3b4b92b5bb3a794f2c3d8023e01e2
Ken Sodemann <ken.sodemann@gmail.com>
2026-04-04 17:24:32 -0500

    WIP - implement the thing

--- src/components/planning/DailySummaryCard.vue
+++ src/components/planning/DailySummaryCard.vue
@@ -11,33 +11,35 @@
     <v-card-title>{{ intlFormat(date, { dateStyle: 'full' }) }}</v-card-title>
     <v-card-subtitle>Nutrition Summary</v-card-subtitle>
     <v-card-text>
-      <!--
-      <div>
-        Days with Meals: <strong>{{ week.daysWithMeals }}</strong>
-      </div>
-      <div>
-        Average Calories: <strong>{{ week.averageCalories }}</strong>
-      </div>
-      <div>
-        Average Protein: <strong>{{ week.averageProtein }}g</strong>
-      </div>
-      <div>
-        Average Carbs: <strong>{{ week.averageCarbs }}g</strong>
-      </div>
-      -->
-      <p>Click to view details</p>
+      <div data-testid="meals-summary">Meals: {{ meals }}</div>
+      <NutritionData v-if="nutrition" :value="nutrition" />
     </v-card-text>
   </v-card>
 </template>
 
 <script setup lang="ts">
 import type { MealPlan } from '@/models/meal-plan';
+import type { Nutrition } from '@/models/nutrition';
 import { intlFormat } from 'date-fns';
+import { dailyMealPlanNutrients } from '@/core/nutritional-calculations';
 
-defineProps<{
+const props = defineProps<{
   date: Date;
   mealPlan?: MealPlan;
 }>();
+
+const buildMealSummary = (mealPlan: MealPlan): string => {
+  let summary = '';
+  if (mealPlan.meals.find((m) => m.type === 'Breakfast')) summary += 'Breakfast';
+  if (mealPlan.meals.find((m) => m.type === 'Lunch')) summary += `${summary ? ', ' : ''}Lunch`;
+  if (mealPlan.meals.find((m) => m.type === 'Dinner')) summary += `${summary ? ', ' : ''}Dinner`;
+  if (mealPlan.meals.find((m) => m.type === 'Snack')) summary += `${summary ? ', ' : ''}Snack`;
+  return summary;
+};
+
+const nutrition: Nutrition | undefined =
+  props.mealPlan && props.mealPlan.meals.length > 0 ? dailyMealPlanNutrients(props.mealPlan) : undefined;
+const meals = props.mealPlan && props.mealPlan.meals.length > 0 ? buildMealSummary(props.mealPlan) : 'None';
 
 defineEmits<{
   (event: 'click'): void;
