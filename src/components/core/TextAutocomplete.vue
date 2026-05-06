abc34a617b4277db258e6a76a336138225fe94ee
Ken Sodemann <ken.sodemann@gmail.com>
2026-05-06 17:11:37 -0500

    WIP - componitize this

---
+++ src/components/core/TextAutocomplete.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-autocomplete
+    v-bind="$attrs"
+    :label="label"
+    v-model="modelValue"
+    v-model:search="search"
+    :items="items"
+    :rules="rules"
+    @keydown.tab="selectFirstItem"
+  ></v-autocomplete>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+  label: string;
+  items: string[];
+  rules: ((value: any) => boolean | string)[];
+}>();
+const modelValue = defineModel<string>('');
+
+const search = ref('');
+
+const selectFirstItem = () => {
+  if (search.value && props.items.length > 0) {
+    modelValue.value = props.items[0];
+  }
+};
+</script>
